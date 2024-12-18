1fb98c9e506f369cdc2bc08ea234bb7f7a2518eb
wa1ead <waliddraa2@gmail.com>
2024-12-18 11:27:18 +0100

    finished

--- app/page.js
+++ app/page.js
@@ -1,13 +1,12 @@
-import Header from "./components/Header";
+import Box from "./components/Box";
 import Hero from "./components/Hero";
-import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <Hero />
-      <Footer />
+      <Box>
+        <Hero />
+      </Box>
     </>
   );
 }
